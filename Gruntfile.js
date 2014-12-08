2cf113e0dde1beea59e1be18e012264a30bb96c3
kyudaitaro <kyudai_taro@yahoo.co.jp>
2014-12-08 19:30:41 +0900

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+var site = require('./settings/site');
+
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.util.linefeed = '\n';
+
+  grunt.initConfig({
+    bower: {
+      install: {
+        options: {
+          targetDir: 'lib',
+          layout: 'byComponent',
+          install: true,
+          verbose: true,
+          cleanTargetDir: true,
+          cleanBowerDir: false
+        }
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            site: site,
+            page: {
+              title: site.name,
+              description: site.description,
+              keywords: site.keywords
+            }
+          }
+        },
+        files: {
+          "index.html": "views/index.jade"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  grunt.registerTask('build', ['bower', 'jade']);
+  grunt.registerTask('default', ['build']);
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# [Kyukou Kyudai](https://kyukou-kyudai.rhcloud.com/) App
+
+[![Dependency Status](https://david-dm.org/Qdai/kyukou-app.svg?style=flat)](https://david-dm.org/Qdai/kyukou-app) [![devDependency Status](https://david-dm.org/Qdai/kyukou-app/dev-status.svg?style=flat)](https://david-dm.org/Qdai/kyukou-app#info=devDependencies)
+
+Alert information about cancellation of a lecture.
+
+NOTICE: This is **Unofficial** web site.
+
+## For development
+
+1. Build: `npm run build`
+
+## Contact
+
+[twitter/kyukou_kyudai](https://twitter.com/kyukou_kyudai)
+
+## License
+
+MIT license
